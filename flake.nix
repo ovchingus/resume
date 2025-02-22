fd1a3be64d3b2baf6ebeeadcf664927b66d8813d
Vladimir Ovechkin <ovchingus@gmail.com>
2025-02-22 19:06:38 +0300

    chore: update build script

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "LaTeX Document Demo";
+  description = "Vladimir Ovechkin LaTeX resume";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib; eachSystem allSystems (system:
+    with flake-utils.lib; eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         tex = pkgs.texlive.combine {
@@ -14,26 +14,22 @@
             latexmk;
         };
       in
-      rec {
-        packages = {
-          document = pkgs.stdenvNoCC.mkDerivation rec {
-            name = "vladimir_ovechkin_resume";
-            src = self;
-            buildInputs = [ pkgs.coreutils tex ];
-            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-            buildPhase = ''
-              export PATH="${pkgs.lib.makeBinPath buildInputs}";
-              mkdir -p .cache/texmf-var
-              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-                latexmk -interaction=nonstopmode -pdf -lualatex \
-                resume.tex
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp resume.pdf $out/
-            '';
-          };
+      {
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          name = "vladimir_ovechkin_resume";
+          src = self;
+          buildInputs = [ tex ];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex \
+              resume.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp resume.pdf $out/$name.pdf
+          '';
         };
-        defaultPackage = packages.document;
       });
 }
